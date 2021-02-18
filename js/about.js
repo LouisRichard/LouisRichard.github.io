ddbed834c96582c98c426329b8b6801bb0a25e66
LouisRichard <louis.richard@tutanota.com>
2021-02-18 20:38:35 +0100

    (WiP)[FEATURE] New cool javascript things in the about page

---
+++ js/about.js
@@ -0,0 +1,11 @@
+// This is taken straight from JonasHau on github
+// https://github.com/JonasHau/JonasHau.github.io
+// https://jonashau.github.io/
+function tab(tab) {
+  document.getElementById("aboutHome").style.display = "none"
+  document.getElementById("programming").style.display = "none"
+  document.getElementById("networking").style.display = "none"
+  document.getElementById("systems").style.display = "none"
+  document.getElementById(tab).style.display = "inline-block"
+}
+tab("aboutHome")
