c90941c667e63bb69e7f7f1386e17c3b8219b39e
LouisRichard <louis.richard@tutanota.com>
2021-02-19 11:59:20 +0100

    [CLEANUP] by codacy advices

--- js/about.js
+++ js/about.js
@@ -2,10 +2,10 @@
 // https://github.com/JonasHau/JonasHau.github.io
 // https://jonashau.github.io/
 function tab(tab) {
-  document.getElementById("aboutHome").style.display = "none"
-  document.getElementById("programming").style.display = "none"
-  document.getElementById("networking").style.display = "none"
-  document.getElementById("systems").style.display = "none"
-  document.getElementById(tab).style.display = "inline-block"
+  document.getElementById("aboutHome").style.display = "none";
+  document.getElementById("programming").style.display = "none";
+  document.getElementById("networking").style.display = "none";
+  document.getElementById("systems").style.display = "none";
+  document.getElementById(tab).style.display = "inline-block";
 }
-tab("aboutHome")
+tab("aboutHome");
