e56cd16c3dcf2b0f58aa89a3a523534db0951c2c
LouisRichard <louis.richard@tutanota.com>
2025-12-10 19:01:01 +0100

    proj/gamelib good enough

--- view/proj/gamelib.php
+++ view/proj/gamelib.php
@@ -14,12 +14,23 @@ ob_start();
       </p>
 
       <p class="mt-4">
-        =lorem()  
+      This project was created within the context of my studies. It was a group project intended to be developed in C# .NET. The subject was free to choose but had to adhere to certain specifications, such as:<br/>
+      * Utilizing a database<br/>
+      * Implementing some form of login functionality</br>
+      * Employing the Agile methodology to manage the project<br/><br/>
+      The intention behind this project was to keep it free and open-source without any restrictions imposed by our school.<br/>
+      Although challenging at times, we managed to achieve this using only free software, with the exception of Visual Studio Enterprise.<br/>
+      The objective was to continuously update the software until we deemed it 'good enough'. However, as time progressed, we became involved in other projects, causing this one to be put aside.<br/>
       </p>
         <br/>
       <h3 class="bracket-text">[DEVELOPPED SKILLS]</h3>
       <p class="util-text">
-        =lorem()
+        C# .NET<br/>
+        .NET Framework<br/>
+        Agile project management<br/>
+        SCRUM<br/>
+        Source control (git)<br/>
+        Doxygen<br/>
       </p>
 
       <h4 class="mt-5 bracket-text">[RESSOURCES]</h4>
