b603228b700dca4cac98ff8b0d16a54103aadfa9
LouisRichard <louis.richard@tutanota.com>
2026-06-25 13:42:27 +0200

    No longer working for Darest

--- view/xp/darest.php
+++ view/xp/darest.php
@@ -9,16 +9,16 @@ ob_start();
       <div class="project-divider"></div>
       <p class="util-text">
         <span class="bracket-text">[Category]</span> IT SERVICE DESK<br>
-        <span class="bracket-text">[Timespan]</span> 2025 - TODAY<br>
-        <span class="bracket-text">[Status]</span> <span style="color:#0F0">ONLINE</span>
+        <span class="bracket-text">[Timespan]</span> FEB. 2025 - AUG. 2026<br>
+        <span class="bracket-text">[Status]</span> <span style="color:#F00">OFFLINE</span>
       </p>
 
       <p class="mt-4">
         Experience aquired as a regular day job.<br/>
-        I joined the IT Service Desk team for the client company. The Service Desk is available 24/7 do so lvl 1 and 2 tech support on workstations and some specialized softwares.<br/>
-        I worked on the phone and at the Service Desk counter in multiple offices.<br/>
-        During my time, I've also worked remotely, sometimes during the night.<br/>
-        In both French and English.<br/>
+        I joined the IT Service Desk team for the client <a href="https://audemarspiguet.com/">Audemars Piguet</a>. The Service Desk is available 24/7 do so lvl 1 and 2 tech support on workstations and some specialized softwares.<br/>
+        I worked on the phone and at the Service Desk counter in multiple offices, in the Vallée de Joux and Crissier.<br/>
+        During my time, I've also worked remotely, sometimes during the night or very early mornings.<br/>
+        Support was provided in both French and English to offices and boutiques across the world.<br/>
         The developped skills list is long, but keep in mind we were level 1 and 2 for end user services. We were also the first point of contact for the entire IT department.<br/>
         Tougher incidents were forwarded to a more specialized L2 or to L3.
       </p>
@@ -41,6 +41,7 @@ ob_start();
         SharePoint<br/>
         OneDrive<br/>
         SamWin call center client<br/>
+        Nimbus Luware call center client<br/>
         Directory Access Management<br/>
         Knowledge base redaction<br/>
         VDI Services<br/>
